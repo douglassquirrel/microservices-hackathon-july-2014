0b10d8e1409c69baa583809aa627363be28b5f51
eduard <eduard.semsch@gmail.com>
2014-07-26 17:02:55 +0100

    sending messages back

--- Java/src/main/java/com/microserviceshack/web/ClientResource.java
+++ Java/src/main/java/com/microserviceshack/web/ClientResource.java
@@ -1,14 +1,28 @@
 package com.microserviceshack.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microserviceshack.web.atmosphere.ChatResource;
+import com.microserviceshack.web.atmosphere.Message;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("/client")
 public class ClientResource {
 
+
+
     @GET
-    public ClientView client() {
-        return new ClientView();
+    public void client(@QueryParam("user") String user) throws JsonProcessingException {
+
+        Broadcaster broadcaster = ChatResource.listeners.get(user);
+        String msg = new ObjectMapper().writeValueAsString(new Message("admin", "Hello!"));
+        broadcaster.broadcast(msg);
+
     }
 
 
--- Java/src/main/java/com/microserviceshack/web/atmosphere/ChatResource.java
+++ Java/src/main/java/com/microserviceshack/web/atmosphere/ChatResource.java
@@ -1,15 +1,23 @@
 package com.microserviceshack.web.atmosphere;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.microserviceshack.Movement;
 import com.microserviceshack.model.Room;
 import com.microserviceshack.repository.RoomManager;
 import org.atmosphere.annotation.Broadcast;
 import org.atmosphere.annotation.Suspend;
+import org.atmosphere.cpr.*;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,18 +28,40 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/")
 public class ChatResource {
+
+    Movement movement = new Movement();
+    RoomManager roomManager = new RoomManager();
+
+    public static Map<String,Broadcaster> listeners = new HashMap<String,Broadcaster>();
+
     @Suspend(contentType = MediaType.APPLICATION_JSON)
     @GET
     public String suspend() {
         return "";
     }
 
+
     @Broadcast(writeEntity = false)
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response broadcast(Message message) {
-        RoomManager roomManager = new RoomManager();
-        Room room = roomManager.getRoom(message.message);
-        return new Response(message.author, room.id+" "+room.room_description);
+    public Response broadcast(Message message, AtmosphereResource ar) throws IOException {
+        if(!listeners.containsKey(message.author)) {
+            Broadcaster broadcaster = BroadcasterFactory.getDefault().lookup("/*");
+            broadcaster.addBroadcasterListener(new BroadcasterListenerAdapter());
+            listeners.put(message.author,broadcaster);
+        }
+
+        if(message.message.startsWith("join")) {
+            movement.sendUserJoinedMessage(message.author);
+            return new Response(message.author,"You joined as: "+message.author);
+        } else if (message.message.startsWith("move")) {
+            String room = message.message.substring("move ".length());
+            movement.sendMoveMessage(message.author, room);
+            return new Response(message.author,"Move to: "+room);
+        } else if(message.message.startsWith("room")) {
+            Room room = roomManager.getRoom(message.message.substring("room ".length()));
+            return new Response(message.author, room.id+" "+room.room_description);
+        }
+        return new Response(message.author, message.message);
     }
 }
