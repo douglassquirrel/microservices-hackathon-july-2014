abb20be67a13a9068f34cbf0c5bd428414c6e09d
eduard <eduard.semsch@gmail.com>
2014-07-26 15:55:20 +0100

    added Atmosphere for websockets

--- Java/src/main/java/com/microserviceshack/web/ClientApplication.java
+++ Java/src/main/java/com/microserviceshack/web/ClientApplication.java
@@ -1,9 +1,11 @@
 package com.microserviceshack.web;
 
+import com.microserviceshack.web.atmosphere.ChatResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+import org.atmosphere.cpr.AtmosphereServlet;
 
 public class ClientApplication extends Application<ClientConfiguration> {
 
@@ -18,7 +20,17 @@ public class ClientApplication extends Application<ClientConfiguration> {
 
     @Override
     public void run(ClientConfiguration clientConfiguration, Environment environment) throws Exception {
+        AtmosphereServlet atmosphereServlet = new AtmosphereServlet();
+        atmosphereServlet.framework().addInitParameter("com.sun.jersey.config.property.packages", "es.microservices.client.atmosphere");
+        atmosphereServlet.framework().addInitParameter("org.atmosphere.websocket.messageContentType", "application/json");
+        atmosphereServlet.framework().addInitParameter("org.atmosphere.cpr.broadcastFilterClasses", "es.microservices.client.atmosphere.BadWordFilter");
+        environment.servlets().addServlet("atmosphere",atmosphereServlet).addMapping("/chat/*");
+
+
         final ClientResource resource = new ClientResource();
         environment.jersey().register(resource);
+
+        final ChatResource chatResource = new ChatResource();
+        environment.jersey().register(chatResource);
     }
 }
