d30ac07f458824ab523009752e10b4455ebfc950
gonka <goncalo.moraes@gmail.com>
2014-07-26 16:32:41 +0100

    RoomManager fixed to select from json.

--- Java/src/main/java/com/microserviceshack/repository/RoomManager.java
+++ Java/src/main/java/com/microserviceshack/repository/RoomManager.java
@@ -1,13 +1,8 @@
 package com.microserviceshack.repository;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microserviceshack.model.Room;
 
-import java.io.IOException;
 import java.sql.*;
-import java.util.Map;
 
 /**
  * Created by gonka on 26/07/2014.
@@ -19,20 +14,14 @@ public class RoomManager {
         try {
             Connection connection = connectToDatabaseOrDie();
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM facts WHERE topic = '" + roomid + "'");
+            ResultSet resultSet = statement.executeQuery("SELECT content->>'room_description' FROM facts WHERE content->>'room_name' = '" + roomid + "'");
             if(resultSet.next()){
-                String json = resultSet.getString(3);
-                ObjectMapper mapper = new ObjectMapper();
-                Map<String,String> response = mapper.readValue(json, Map.class);
-                return new Room(resultSet.getString(1), response.get("room_description") );
+                String result = resultSet.getString(1);
+//                ObjectMapper mapper = new ObjectMapper();
+//                Map<String,String> response = mapper.readValue(json, Map.class);
+                return new Room(roomid, result );
             }
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (JsonParseException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
         throw new RuntimeException("room not found!");
@@ -65,7 +54,7 @@ public class RoomManager {
      * test
      */
     public static void main(String[] args) {
-        Room room_created = new RoomManager().getRoom("room_created");
+        Room room_created = new RoomManager().getRoom("Atrium");
         System.out.println(room_created.room_description);
     }
 
